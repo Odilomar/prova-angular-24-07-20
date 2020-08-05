7a328d2f49b9e09256ad15ca8eb491236bad7f27
Odilomar Rebelo Rocha Junior <odilomar.junior@gmail.com>
2020-08-05 16:52:56 -0400

    :bug: CPF was not saving with zero in front of the number

--- src/app/core/components/createedit/createedit.component.ts
+++ src/app/core/components/createedit/createedit.component.ts
@@ -3,6 +3,7 @@ import { Person } from "../../interfaces/person.interface";
 import { v4 as uuidv4 } from "uuid";
 import { PersonsService } from "../../services/persons.service";
 import { ActivatedRoute, Router } from "@angular/router";
+import { CepService } from "../../services/cep.service";
 
 @Component({
   selector: "app-createedit",
@@ -11,11 +12,44 @@ import { ActivatedRoute, Router } from "@angular/router";
 })
 export class CreateeditComponent implements OnInit {
   public selectedPerson: Person;
+  public loading: boolean;
 
-  constructor(private personsService: PersonsService, private route: Router) {}
+  constructor(
+    private personsService: PersonsService,
+    private route: Router,
+    private cepService: CepService
+  ) {}
 
   ngOnInit() {
     this.selectedPerson = this.personsService.getDefaultPerson();
+    this.loading = false;
+  }
+
+  public changeCep(event) {
+    var cep = event.target.value;
+    if (cep.length == 8) {
+      this.loading = true;
+      this.cepService
+        .getCep(cep)
+        .then((apiResponse: any) => {
+          if (apiResponse.erro) {
+            alert("Cep não encontrado");
+          } else {
+            this.selectedPerson = {
+              ...this.selectedPerson,
+              cep: apiResponse.cep.replace("-", ""),
+              state: apiResponse.uf,
+              city: apiResponse.localidade,
+              street: apiResponse.logradouro,
+            };
+          }
+        })
+        .catch((error) => {
+          alert("Erro ao buscar o cep");
+          console.error(error);
+        })
+        .finally(() => (this.loading = false));
+    }
   }
 
   public save() {
@@ -30,7 +64,6 @@ export class CreateeditComponent implements OnInit {
   }
 
   private validateForm(): boolean {
-
     if (this.selectedPerson.name == "") {
       alert("Campo de nome não informado. Informe-o e tente novamente!");
       return false;
