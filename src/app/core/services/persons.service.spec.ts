0e7c4b9e3a0438244e04a0150ed969976294a674
Odilomar Rebelo Rocha Junior <odilomar.junior@gmail.com>
2020-08-09 15:23:45 -0400

    :white_check_mark: Create PersonService Tests

--- src/app/core/services/persons.service.spec.ts
+++ src/app/core/services/persons.service.spec.ts
@@ -1,12 +1,111 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { PersonsService } from './persons.service';
+import { PersonsService } from "./persons.service";
+import { Person } from "../interfaces/person.interface";
 
-describe('PersonsService', () => {
+import { v4 as uuidv4 } from "uuid";
+import { create } from "domain";
+
+type PersonWhitoutID = Omit<Person, "id">;
+
+describe("PersonsService", () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: PersonsService = TestBed.get(PersonsService);
-    expect(service).toBeTruthy();
+  it("should be created", () => {
+    const personService: PersonsService = TestBed.get(PersonsService);
+    expect(personService).toBeTruthy();
   });
+  it("should populate and retrive all persons", () => {
+    const personService: PersonsService = TestBed.get(PersonsService);
+    personService.populateTable();
+    const persons = personService.getAllPersons().map((person) => {
+      delete person.id;
+      return person as PersonWhitoutID;
+    });
+    const defaultPersons: PersonWhitoutID[] = [
+      {
+        name: "Maria Flores",
+        cpf: "83321492075",
+        phone: "1533283928",
+        email: "maria_f@gmail.com",
+        cep: "69906043",
+        state: "AC",
+        city: "Rio Branco",
+        street: "Rua Arnaldo Aleixo (Conjunto Canaã)",
+      },
+      {
+        name: "João Carlos",
+        cpf: "31213393035",
+        phone: "1532841040",
+        email: "joao.c@gmail.com",
+        cep: "79096766",
+        state: "MS",
+        city: "Campo Grande",
+        street: "Rua Rodolfho José Rospide da Motta",
+      },
+      {
+        name: "Stephanie Dias",
+        cpf: "02085196020",
+        phone: "1533294040",
+        email: "ste.dias@gmail.com",
+        cep: "23825080",
+        state: "RJ",
+        city: "Itaguaí",
+        street: "Rua Mario Bastos Filho",
+      },
+      {
+        name: "Mirtes Souza",
+        cpf: "33764389001",
+        phone: "1530178756",
+        email: "irma.mirtes@gmail.com",
+        cep: "40420150",
+        state: "BA",
+        city: "Salvador",
+        street: "1ª Travessa Clóvis de Almeida Maia",
+      },
+    ];
+    expect(persons).toEqual(defaultPersons);
+  });
+
+  it("should be able to create a person", () => {
+    const personService: PersonsService = TestBed.get(PersonsService);
+    const person = createPerson();
+    const { id } = person;
+    expect(personService.getPersonById(id)).toEqual(person);
+  });
+
+  it("should be able to edit a person", () => {
+    const personService: PersonsService = TestBed.get(PersonsService);
+    const savedPerson = createPerson();
+    const person: Person = { ...savedPerson, cpf: "78303355163" };
+    const { id } = savedPerson;
+    personService.savePerson(person);
+    expect(personService.getPersonById(id)).toEqual(person);
+  });
+
+  it("should be able to delete a person", () => {
+    const personService: PersonsService = TestBed.get(PersonsService);
+    const { id } = createPerson();
+    personService.deletePersonById(id);
+    expect(personService.isPersonById(id)).toEqual(false);
+  });
+
+  function createPerson(): Person {
+    const personService: PersonsService = TestBed.get(PersonsService);
+    const person: Person = {
+      id: uuidv4(),
+      name: "Raul César Drumond",
+      cpf: "69599933699",
+      phone: "82994940448",
+      email: "rrebecalouisealmeida@robertacorrea.com",
+      cep: "57010788",
+      city: "Maceió",
+      state: "AL",
+      street: "Rua N",
+    };
+
+    personService.savePerson(person);
+
+    return person;
+  }
 });
